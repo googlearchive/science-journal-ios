1f3200b34730bcbe516e7d4f7599e9d991270ade
Bill Griesau <billgriesau@google.com>
2019-04-18 18:51:57 +0000

    Update generate strings script to output Google LLC rather than Inc
    PiperOrigin-RevId: 244229943

--- Scripts/generate_strings.py
+++ Scripts/generate_strings.py
@@ -7,7 +7,7 @@
 """
 
 ##
-#  Copyright 2019 Google Inc. All Rights Reserved.
+#  Copyright 2019 Google LLC. All Rights Reserved.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -31,7 +31,7 @@ import sys
 
 
 GENERATED_STRINGS_TEMPLATE = string.Template("""/*
- *  Copyright ${year} Google Inc. All Rights Reserved.
+ *  Copyright ${year} Google LLC. All Rights Reserved.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.

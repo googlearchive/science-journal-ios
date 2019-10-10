5cee9e79cace997b419be15532e197543527beea
Noah Emmet <noah@noahemmet.com>
2019-10-10 13:42:17 -0700

    Fix card offset animation when recording

--- ScienceJournal/UI/ObserveViewController.swift
+++ ScienceJournal/UI/ObserveViewController.swift
@@ -884,6 +884,10 @@ open class ObserveViewController: ScienceJournalCollectionViewController, ChartC
           UIView.layoutFittingCompressedSize).height : 0
       collectionView?.contentInset.top = topInset
       collectionView?.scrollIndicatorInsets.top = topInset
+      if collectionView?.contentOffset.y == 0 {
+        // Adjusts the offset so the timeAxisView doesn't obscure the top-most card
+        collectionView?.contentOffset.y = -topInset
+      }
     } else {
       var bottomInset =
         recordButtonViewWrapper.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
@@ -898,10 +902,16 @@ open class ObserveViewController: ScienceJournalCollectionViewController, ChartC
 
   // Animates the display of the time axis view.
   func showTimeAxisView(_ isVisible: Bool) {
-    UIView.animate(withDuration: 0.5, animations: {
-      self.timeAxisController.timeAxisView.alpha = isVisible ? 1 : 0
-    }) { (_) in
-      self.adjustContentInsets()
+    if FeatureFlags.isActionAreaEnabled {
+      self.transitionCoordinator?.animate(alongsideTransition: { _ in
+        self.timeAxisController.timeAxisView.alpha = isVisible ? 1 : 0
+        self.adjustContentInsets()
+      }, completion: nil)
+    } else {
+      UIView.animate(withDuration: 0.4) {
+        self.timeAxisController.timeAxisView.alpha = isVisible ? 1 : 0
+        self.adjustContentInsets()
+      }
     }
   }
 
